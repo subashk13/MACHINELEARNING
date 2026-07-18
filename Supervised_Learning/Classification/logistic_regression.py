59d6a05481c8cbbc9b828ad56035102518d686dd
SubashK13 <subashkamalakannan03@gmail.com>
2026-07-18 20:44:42 +0530

    logistics

---
+++ Supervised_Learning/Classification/logistic_regression.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Create dataset
+X, y = make_classification(
+    n_samples=100,
+    n_features=5,
+    random_state=42
+)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Create model
+model = LogisticRegression()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Evaluation
+print("Accuracy:", accuracy_score(y_test, y_pred))
