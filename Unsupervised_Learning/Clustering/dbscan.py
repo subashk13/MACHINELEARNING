dc6d7a8b2102bc0a165b675baccc64caf6177604
SubashK13 <subashkamalakannan03@gmail.com>
2026-08-03 20:27:34 +0530

    dbscan

---
+++ Unsupervised_Learning/Clustering/dbscan.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
+
+# Create dataset
+X, _ = make_blobs(
+    n_samples=300,
+    centers=4,
+    cluster_std=0.60,
+    random_state=42
+)
+
+# Create model
+model = DBSCAN(
+    eps=0.5,
+    min_samples=5
+)
+
+# Train model
+labels = model.fit_predict(X)
+
+# Evaluation
+if len(set(labels)) > 1 and -1 not in set(labels):
+    print("Silhouette Score:", silhouette_score(X, labels))
+else:
+    print("Silhouette Score cannot be calculated.")
+    
